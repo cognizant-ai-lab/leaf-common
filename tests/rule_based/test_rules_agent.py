8db8c6fae4d42dad2f8e04449433a9771c681f36
darren.sargent <darren.sargent@cognizant.ai>
2020-04-15 16:51:51 -0700

    Clean up mock usage by using patch Context Manager
    Also move tests to folders to mirror app code

--- tests/rule_based/test_rules_agent.py
+++ tests/rule_based/test_rules_agent.py
@@ -10,7 +10,7 @@ from leaf_common.rule_based.rule import THE_ACTION
 from leaf_common.rule_based.rules_agent import RulesAgent
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-FIXTURES_PATH = os.path.join(ROOT_DIR, 'fixtures')
+FIXTURES_PATH = os.path.join(ROOT_DIR, '..', 'fixtures')
 
 
 class TestRulesAgent(TestCase):
@@ -47,15 +47,13 @@ class TestRulesAgent(TestCase):
         self.assertIsNot(agent, reloaded_agent)
         self.assertEqual(agent, reloaded_agent)
 
-    @patch('leaf_common.rule_based.rules_agent.Rule', autospec=True)
-    @patch('leaf_common.rule_based.rules_agent.random.randint', autospec=True)
-    def test_parse_rules_agree(self, mock_randint, mock_rule):
+    def test_parse_rules_agree(self):
         """
         Verify correct parsing of rules
         """
 
         # Set it up so mock rules agree on action1
-        agent, num_rules = self._create_rules_agent(mock_randint, mock_rule, rule1_action={THE_ACTION: 'action1'},
+        agent, num_rules = self._create_rules_agent(rule1_action={THE_ACTION: 'action1'},
                                                     rule2_action={THE_ACTION: 'action1'})
 
         self.assertEqual(num_rules, len(agent.rules))
@@ -67,15 +65,13 @@ class TestRulesAgent(TestCase):
         self.assertEqual(num_rules, result['action1'])
         self.assertEqual(0, result['action2'])
 
-    @patch('leaf_common.rule_based.rules_agent.Rule', autospec=True)
-    @patch('leaf_common.rule_based.rules_agent.random.randint', autospec=True)
-    def test_parse_rules_disagree(self, mock_randint, mock_rule):
+    def test_parse_rules_disagree(self):
         """
         Verify correct parsing of rules
         """
 
         # Set it up so mock rules vote differently -- 1 for action1, 1 for action2
-        agent, num_rules = self._create_rules_agent(mock_randint, mock_rule, rule1_action={THE_ACTION: 'action1'},
+        agent, num_rules = self._create_rules_agent(rule1_action={THE_ACTION: 'action1'},
                                                     rule2_action={THE_ACTION: 'action2'})
 
         self.assertEqual(num_rules, len(agent.rules))
@@ -88,17 +84,19 @@ class TestRulesAgent(TestCase):
         self.assertEqual(1, result['action2'])
 
     @staticmethod
-    def _create_rules_agent(mock_randint, mock_rule, rule1_action, rule2_action):
-        mock_rule_1 = MagicMock()
-        mock_rule_2 = MagicMock()
-        mock_rule_1.conditions = [MagicMock()]
-        mock_rule_2.conditions = [MagicMock()]
-        mock_rule_1.parse.return_value = rule1_action
-        mock_rule_2.parse.return_value = rule2_action
-        mock_rule.side_effect = [mock_rule_1, mock_rule_2]
-        num_rules = 2
-        mock_randint.return_value = num_rules
-        agent = RulesAgent(states={'k1': 'value1', 'k2': 'value2'},
-                           actions={'action1': 'action_value1', 'action2': 'action_value2'},
-                           initial_state={'state1': 'value1'})
-        return agent, num_rules
+    def _create_rules_agent(rule1_action, rule2_action):
+        with patch('leaf_common.rule_based.rules_agent.Rule', autospec=True) as mock_rule, \
+                patch('leaf_common.rule_based.rules_agent.random.randint', autospec=True) as mock_randint:
+            mock_rule_1 = MagicMock()
+            mock_rule_2 = MagicMock()
+            mock_rule_1.conditions = [MagicMock()]
+            mock_rule_2.conditions = [MagicMock()]
+            mock_rule_1.parse.return_value = rule1_action
+            mock_rule_2.parse.return_value = rule2_action
+            mock_rule.side_effect = [mock_rule_1, mock_rule_2]
+            num_rules = 2
+            mock_randint.return_value = num_rules
+            agent = RulesAgent(states={'k1': 'value1', 'k2': 'value2'},
+                               actions={'action1': 'action_value1', 'action2': 'action_value2'},
+                               initial_state={'state1': 'value1'})
+            return agent, num_rules
