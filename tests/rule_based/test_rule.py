4c5126f6a27e5726b86b1ba13502fee420b708e4
Dan Fink <Dan.Fink@cognizant.com>
2020-06-29 17:43:23 -0700

    remove pathc

--- tests/rule_based/test_rule.py
+++ tests/rule_based/test_rule.py
@@ -2,10 +2,12 @@
 Unit tests for Rules class
 """
 from unittest import TestCase
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock
 
-from leaf_common.rule_based.condition import THE_MIN, THE_MAX
-from leaf_common.rule_based.rule import Rule, NO_ACTION
+from leaf_common.rule_based.condition import THE_MIN
+from leaf_common.rule_based.condition import THE_MAX
+from leaf_common.rule_based.rule import Rule
+from leaf_common.rule_based.rule import NO_ACTION
 
 MIN_MAXES = {('0', THE_MIN): 0, ('0', THE_MAX): 10, ('1', THE_MIN): 10, ('1', THE_MAX): 20}
 DOMAIN_STATES = [{'0': 0.0, '1': 15.0}, {'0': 0.5, '1': 16.0}]
@@ -39,20 +41,21 @@ class TestRule(TestCase):
 
     @staticmethod
     def _create_rule(first_condition_true, second_condition_true):
-        with patch('leaf_common.rule_based.rule.Condition', autospec=True) as mock_condition:
-            mock_condition_1 = MagicMock()
-            mock_condition_1.parse.return_value = first_condition_true
-            mock_condition_1.action = 'action1'
-            mock_condition_1.get_str.return_value = 'condition1_str'
-            mock_condition_2 = MagicMock()
-            mock_condition_2.parse.return_value = second_condition_true
-            mock_condition_2.action = 'action2'
-            mock_condition_2.get_str.return_value = 'condition2_str'
 
-            rule = Rule(actions={'0': 'action1', '1': 'action2'}, max_lookback=0)
-            rule.action = '1'
-            rule.action_lookback = 0
-            rule.add_condition(mock_condition_1)
-            rule.add_condition(mock_condition_2)
+        mock_condition_1 = MagicMock()
+        mock_condition_1.parse.return_value = first_condition_true
+        mock_condition_1.action = 'action1'
+        mock_condition_1.get_str.return_value = 'condition1_str'
 
-            return rule
+        mock_condition_2 = MagicMock()
+        mock_condition_2.parse.return_value = second_condition_true
+        mock_condition_2.action = 'action2'
+        mock_condition_2.get_str.return_value = 'condition2_str'
+
+        rule = Rule(actions={'0': 'action1', '1': 'action2'}, max_lookback=0)
+        rule.action = '1'
+        rule.action_lookback = 0
+        rule.add_condition(mock_condition_1)
+        rule.add_condition(mock_condition_2)
+
+        return rule
