8105c7c0facfa0b0444595c7f487e24277badbd1
Dan Fink <Dan.Fink@cognizant.com>
2020-09-23 11:48:10 -0700

    add some comments as to where members might really belong

--- leaf_common/representation/rule_based/rules_agent.py
+++ leaf_common/representation/rule_based/rules_agent.py
@@ -14,21 +14,31 @@ class RulesAgent:
 
     def __init__(self, states, actions, initial_state, uid="rule_based"):
 
-        # State/Config needed for evaluation
+        # We might be able to leave uid to the service infrastructure
         self.uid = uid
+
+        # Are these only used in evaluation?
         self.state = initial_state
         self.state[RulesEvaluationConstants.AGE_STATE_KEY] = 0
+
+        # These might be able to come from config as opposed to
+        # being trucked around by each individual over the wire
         self.actions = actions
         self.states = states
+
+        # Metric used in reproduction
         self.times_applied = 0
 
+        # The only thing keeping these state_min_maxes here as opposed to the
+        # RuleSetEvaluator is that they are used in __str__() below. Otherwise
+        # it seems they could move to RuleSetEvaluator.reset()
         self.state_min_maxes = {}
         for state in self.states.keys():
             self.state_min_maxes[state, RulesEvaluationConstants.MIN_KEY] = 0
             self.state_min_maxes[state, RulesEvaluationConstants.MAX_KEY] = 0
             self.state_min_maxes[state, RulesEvaluationConstants.TOTAL_KEY] = 0
 
-        # Genetic Material
+        # Honest-to-goodness Genetic Material
         self.default_action = None
         self.rules = []
 
