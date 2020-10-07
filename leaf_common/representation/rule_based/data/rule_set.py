c1b5f0b375cd210843ecf95040b835c4939dec41
Dan Fink <Dan.Fink@cognizant.com>
2020-10-06 17:04:54 -0700

    min/max propagation

--- leaf_common/representation/rule_based/data/rule_set.py
+++ leaf_common/representation/rule_based/data/rule_set.py
@@ -12,7 +12,18 @@ class RuleSet:
     # pylint: disable=too-many-instance-attributes
     # Nine is reasonable in this case.
 
-    def __init__(self):
+    def __init__(self, min_maxes: Dict[Tuple[str, str], float] = None):
+        """
+        Constructor
+
+        :param min_maxes: A dictionary of (state, "min"/"max") to a float value
+                    which pre-calibrates the normalization of the conditions.
+                    These values are copied and as evaluation proceeds, the
+                    internal copy gets updated with new values should the
+                    data encountered warrant it.  The default value is None,
+                    indicating that we don't know enough about the data to
+                    calibrate anything at the outset.
+        """
 
         # Evaluation Metrics used in reproduction
         self.times_applied = 0
@@ -22,17 +33,23 @@ class RuleSet:
         self.default_action = None
         self.rules = []
 
+        # Initialize the min/maxes
+        if min_maxes is not None:
+            self.min_maxes = deepcopy(min_maxes)
+        else:
+            self.min_maxes = {}
+
     # see https://github.com/PyCQA/pycodestyle/issues/753 for why next line needs noqa
-    def to_string(self, states: Dict[str, str] = None,
-                  state_min_maxes: Dict[Tuple[str, str], float] = None) -> str:  # noqa: E252
+    def to_string(self, states: Dict[str, str] = None) -> str:      # noqa: E252
         """
         String representation for rule
-        :param min_maxes: A dictionary of domain features minimum and maximum values
+
+        :param states: An optional dictionary of state definitions seen during evaluation.
         :return: RuleSet.toString()
         """
         rules_str = ""
         for rule in self.rules:
-            rules_str = rules_str + rule.to_string(states, state_min_maxes) + "\n"
+            rules_str = rules_str + rule.to_string(states, self.min_maxes) + "\n"
         times_applied = " <> "
         if self.times_applied > 0:
             times_applied = " <" + str(self.times_applied) + "> "
