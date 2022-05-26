fb05b67636765b787b1e34040f49932b7ac2df1d
Dan Fink <Daniel.Fink@cognizant.com>
2022-05-26 14:17:07 -0700

    fix pylint

--- leaf_common/session/grpc_channel_security.py
+++ leaf_common/session/grpc_channel_security.py
@@ -39,6 +39,7 @@ class GrpcChannelSecurity():
     reading key/value pairs from a security configuration dictionary.
     """
 
+    # pylint: disable=too-many-arguments
     def __init__(self, security_cfg: Dict[str, Any] = None,
                  auth0_defaults: Dict[str, Any] = None,
                  service_name: str = None,
