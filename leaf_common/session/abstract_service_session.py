cc1fa8b7cbbe9db7377bdefa95623e86ee75cae6
Dan Fink <Daniel.Fink@cognizant.com>
2024-11-12 07:47:39 -0800

    Fix pylint

--- leaf_common/session/abstract_service_session.py
+++ leaf_common/session/abstract_service_session.py
@@ -1,4 +1,18 @@
 
+# Copyright (C) 2019-2023 Cognizant Digital Business, Evolutionary AI.
+# All Rights Reserved.
+# Issued under the Academic Public License.
+#
+# You can be released from the terms, and requirements of the Academic Public
+# License by purchasing a commercial license.
+# Purchase of a commercial license is mandatory for any use of the
+# leaf-common SDK Software in commercial settings.
+#
+# END COPYRIGHT
+"""
+See class comment for details.
+"""
+
 from typing import Any
 from typing import Dict
 from typing import List
